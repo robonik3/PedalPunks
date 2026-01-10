2e01b06bcff5444dff6dad24bf0fd76cb5ce93e7
ghimirerush <ghimirerush@gmail.com>
2026-01-10 16:44:07 -0500

    commit my changes

--- TSAVGD_25-26/Assets/Scenes/Aizen Test Scene/CarScript.cs
+++ TSAVGD_25-26/Assets/Scenes/Aizen Test Scene/CarScript.cs
@@ -1,32 +1,61 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class CarScript : MonoBehaviour
 {
-    private Rigidbody2D mover;
-    //[SerializeField] private BikeType bike;
-    //private bool crash;
+    public Sprite[] carSprites;
+    public float minSpeed = 3f;
+    public float maxSpeed = 7f;
+    float speed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        mover = GetComponent<Rigidbody2D>();
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        sr.sprite = carSprites[Random.Range(0, carSprites.Length)];
+
+        Debug.Log("Car Spawned");
+        speed = Random.Range(minSpeed, maxSpeed);
+        //Spawn
+        int randomNum = Random.Range(0,4);
+        float Yposition = 0f;
+        if(randomNum == 0)
+        {
+            Yposition = -2.2f;
+        }
+        if(randomNum == 1)
+        {
+            Yposition = -1.2f;
+        }
+        if(randomNum == 2)
+        {
+            Yposition = -0.1f;
+        }    
+        if(randomNum == 3)
+        {
+            Yposition = 0.8f;
+        }
+
+        transform.position = new Vector3(12f,Yposition, 0f);
+
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-            mover.linearVelocityX = -2;
-            
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
 
-            if (transform.position.x < -10) { Dead(); }
-
-            
+        if(transform.position.x < -12f)
+        {
+            Destroy(gameObject);
         }
-    
-    void Dead()
-    {
-        CarSpawner.instance.activeCars--;
-        Destroy(gameObject);
     }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
 }
--- TSAVGD_25-26/Assets/Scenes/Aizen Test Scene/CarSpawner.cs
+++ TSAVGD_25-26/Assets/Scenes/Aizen Test Scene/CarSpawner.cs
@@ -1,64 +1,17 @@
 using UnityEngine;
-using System.Collections;
-using UnityEngine.Events;
+
 public class CarSpawner : MonoBehaviour
 {
-    public static CarSpawner instance;
+    public GameObject carPrefab;
+    public float spawnDelay = 3f;
 
-    [SerializeField] private WaveInfo[] waves;
-    private int runthrough;
-
-    [HideInInspector] public int activeCars;
-    //private int completedWaves;
-    private float timer;
-    //public bool readyToEndLevel;
-
-    [System.Serializable]
-    public class WaveInfo
-    {
-        public GameObject car;
-        public float delaySpawnTime;
-        public float ypos;
-    }
-    public UnityEvent FinishLevel;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        instance = this;
-        for (int i = 0; i < waves.Length; i++)
-        {
-            StartCoroutine("WaitToSpawn");
-            runthrough++;
-        }
+        InvokeRepeating(nameof(SpawnCar), 1f, spawnDelay);
     }
-    private void Update()
+
+    void SpawnCar()
     {
-        timer += Time.deltaTime;
-        if (activeCars < 1)
-        {
-            if (5 < timer && timer < 120)
-            {
-                timer += 3;
-            }
-
-
-                    if (timer > 126)
-                    {
-                        timer -= 4;
-                        Instantiate(waves[0].car, new Vector3(10, 1f, 0), new Quaternion());
-                    }
-        }
+        Instantiate(carPrefab);
     }
-    IEnumerator WaitToSpawn()
-    {
-        int i = runthrough;
-        while (timer < waves[i].delaySpawnTime)
-        {
-            yield return null;
-        }
-        activeCars++;
-        Instantiate(waves[i].car, new Vector3(10, waves[i].ypos, 0), new Quaternion());
-    }
-
 }
