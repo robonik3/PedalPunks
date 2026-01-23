990ea20f2a42bd2bb3af8581f5b7a6fa2dd31680
Mic4321 <100938498+Mic4321@users.noreply.github.com>
2026-01-22 19:01:52 -0500

    Minor edits

--- TSAVGD_25-26/Assets/PauseCode.cs
+++ TSAVGD_25-26/Assets/PauseCode.cs
@@ -1,9 +1,12 @@
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
 public class PauseCode : MonoBehaviour
 {
     [SerializeField] GameObject pauseScreen;
+     //public GameObject pauseButton;
     bool isOn = false;
 
     void Update()
@@ -24,6 +27,7 @@ public class PauseCode : MonoBehaviour
     public void SetPause()
     {
         pauseScreen.SetActive(true);
+        //pauseButton.SetActive(false);
         Time.timeScale = 0f; //Need to fix
         isOn=true;
     }
@@ -31,18 +35,23 @@ public class PauseCode : MonoBehaviour
     public void SetResume()
     {
         pauseScreen.SetActive(false);
-        
+       // pauseButton.SetActive(true);
+        Time.timeScale = 1f;
         isOn=false;
     }
 
     public void GoHome()
     {
         SceneManager.LoadScene("Menu");
+        Time.timeScale = 1f;
+        //pauseButton.SetActive(true);
     }
 
     public void GoLevel()
     {
         SceneManager.LoadScene("LevelSelect");
+        Time.timeScale = 1f;
+        //pauseButton.SetActive(true);
     }
 
 }
