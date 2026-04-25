acb0db94578fe6e3e19c40df904290bd4ee30c5f
MOMANDZENTV <momandzentv@gmail.com>
2026-04-25 19:53:47 -0400

    Random Stuff when I built game

--- TSAVGD_25-26/Assets/Scripts/CutScript.cs
+++ TSAVGD_25-26/Assets/Scripts/CutScript.cs
@@ -19,7 +19,7 @@ public class CutScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P)) { timer = length; }
+        if (Input.GetKeyDown(KeyCode.Return)||Input.GetKeyDown(KeyCode.KeypadEnter)) { timer = length; }
         timer+=Time.deltaTime;
 
         if (timer>=length)
